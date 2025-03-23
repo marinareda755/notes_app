826fef3976f1a374bcdf882c55d5cfc3dfe64aa6
marinareda755 <marinareda2022@gmail.com>
2025-03-23 15:44:52 +0200

    delete note done

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -22,14 +22,14 @@ class NoteItem extends StatelessWidget {
           left: 16,
         ),
         decoration: BoxDecoration(
-          color:  Color(note.color),
+          color: Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             ListTile(
-              title:  Text(
+              title: Text(
                 note.title,
                 style: const TextStyle(color: Colors.black, fontSize: 26),
               ),
@@ -42,7 +42,9 @@ class NoteItem extends StatelessWidget {
                 ),
               ),
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  note.delete();
+                },
                 icon: const Icon(
                   FontAwesomeIcons.trash,
                   color: Colors.black,
